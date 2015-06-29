c973801abf750d511dfe85bc54423b03090e1ed8
Erika Pauwels <erika.pauwels@tenforce.com>
2015-06-29 16:29:01 +0200

    Add master API to Tomcat webapps

--- startup.sh
+++ startup.sh
@@ -2,6 +2,8 @@
 chown -R tomcat7:tomcat7 /unified-views
 chown -R tomcat7:tomcat7 /logs
 
+
+# Database connection details
 cd /config
 ORIG_URL="database.sql.url  = jdbc:mysql:\/\/mysql:3306\/unified_views?characterEncoding=utf8"
 ORIG_USER="database.sql.user = unified_views"
@@ -13,11 +15,17 @@ sed -i "s/$ORIG_URL/$URL/" *.properties
 sed -i "s/$ORIG_USER/$USER/" *.properties
 sed -i "s/$ORIG_PASSWORD/$PASSWORD/" *.properties
 
+# Unified Views backend
 cd /unified-views
-
 nohup java -DconfigFileLocation=/config/backend-config.properties -jar /packages/backend-2.1.0.jar &
 
+# Unified Views frontend
 cp /packages/unifiedviews.war /var/lib/tomcat7/webapps/
+
+# Unified Views API
+cp /packages/master.war /var/lib/tomcat7/webapps/
+
+# Start Tomcat
 chmod +x /usr/share/tomcat7/bin/setenv.sh
 service tomcat7 start
 
