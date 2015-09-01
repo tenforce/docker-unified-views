cd4bdf074dc58be75b6c8332101a5074fabf53b6
Erika Pauwels <erika.pauwels@tenforce.com>
2015-09-01 10:40:48 +0200

    Increase memory for master API

--- tomcat-setenv.sh
+++ tomcat-setenv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-JAVA_OPTS="${JAVA_OPTS} -DconfigFileLocation=/config/frontend-config.properties "
+JAVA_OPTS="${JAVA_OPTS} -DconfigFileLocation=/config/frontend-config.properties -Xmx512M"
