daaffa8f60b8ff1ef635da4334f26887100933b9
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2016-07-12 11:18:34 +0000

    repackage the war 	modified:   init.sh

--- uv-frontend/deployment/init.sh
+++ uv-frontend/deployment/init.sh
@@ -1,5 +1,5 @@
 chmod 777 /unified-views/logs
 chmod 777 /unified-views/dpu
 
-cd /webapps/unpacked/unifiedviews_2.3.1
-fastjar -cf ../unifiedviews_2.3.1.war
+cd /webapps/unpacked/unifiedviews-2.3.1
+fastjar -cf /webapps/unifiedviews_2.3.1.war *
