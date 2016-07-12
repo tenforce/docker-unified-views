d398884ff89d7bee78d68fa26989b8bfdfcb837a
Bert Van Nuffelen <bert.vannuffelen@gmail.com>
2016-07-12 11:53:40 +0000

    apply port 	modified:   startup.sh

--- uv-add-dpus/startup.sh
+++ uv-add-dpus/startup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 # Wait till Tomcat startup has finished and webapps are started (max 3 minutes)
+if [ "$UV_PORT" != ""] ; then
+   FRONTENDPORT=$UV_PORT
+else 
+   FRONTENDPORT=8080
+fi
+
 i=0
-until $(curl --output /dev/null --silent --head --fail --user $MASTER_USER:$MASTER_PASSWORD http://frontend:8080/master/api/1/pipelines) || [ "$i" -gt 36 ]; do
+until $(curl --output /dev/null --silent --head --fail --user $MASTER_USER:$MASTER_PASSWORD http://frontend:$FRONTENDPORT/master/api/1/pipelines) || [ "$i" -gt 36 ]; do
     i=$((i+1))
     printf '.'
     sleep 5
