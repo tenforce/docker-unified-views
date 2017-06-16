61ead93d408aa679912b7edd35bb5a576f1caf59
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-06-16 08:28:51 +0000

    adjust for docker-compose
    	modified:   Dockerfile
    	renamed:    env-to-java-properties-file.sh -> config/env-to-java-properties-file.sh
    	new file:   config/start.sh

--- uv-shared/config/env-to-java-properties-file.sh
+++ uv-shared/config/env-to-java-properties-file.sh
@@ -8,15 +8,15 @@ then
     mkdir -p /unified-views/backend/working/temp/export /unified-views/backend/working/temp/import
     chmod 777 /unified-views/backend/working/temp/export
     chmod 777 /unified-views/backend/working/temp/import
-    cp /etc/unifiedviews/config/config.properties /config/frontend-config.properties
-    cp /etc/unifiedviews/config/config.properties /config/backend-config.properties
+    cp /etc/unifiedviews/config/config.properties /unified-views/config/frontend-config.properties
+    cp /etc/unifiedviews/config/config.properties /unified-views/config/backend-config.properties
     printenv | grep -P "^UV_" | while read setting
     do
 	key=`echo "$setting" | grep -o -P "^UV_[^=]+" | sed 's/^.\{3\}//g' | sed 's/_/./g' | awk '{print tolower($0)}'`
 	value=`echo "$setting" | grep -o -P "=.*$" | sed 's/^=//g'`
 	echo "Registering $key to be $value"
-	echo "$key=$value" >> /config/frontend-config.properties
-	echo "$key=$value" >> /config/backend-config.properties
+	echo "$key=$value" >> /unified-views/config/frontend-config.properties
+	echo "$key=$value" >> /unified-views/config/backend-config.properties
     done
     touch /.settings_set
 fi
