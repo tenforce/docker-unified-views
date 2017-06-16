aed764c32cf227f9acf23a3252b2baccd96f7982
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-06-16 08:31:21 +0000

    fix adjust for docker-compose 	modified:   Dockerfile 	modified:   tomcat-setenv.sh

--- uv-frontend/tomcat-setenv.sh
+++ uv-frontend/tomcat-setenv.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-
-JAVA_OPTS="${JAVA_OPTS} -DconfigFileLocation=/config/frontend-config.properties -Xmx1g -Xms512m"
+if [ -f /config/frontend-config.properties ] ; then
+    echo "using /config/backend-config.properties"
+    JAVA_OPTS="${JAVA_OPTS} -DconfigFileLocation=/config/frontend-config.properties -Xmx1g -Xms512m"
+else 
+    echo "using /unified-views/config/backend-config.properties"
+    JAVA_OPTS="${JAVA_OPTS} -DconfigFileLocation=/unified-views/config/frontend-config.properties -Xmx1g -Xms512m"
+fi
